723cb8a7d903a094700d4846338c3f4f238a995d
Duncan Lowrie <dunc@duncster.co.uk>
2018-11-26 08:03:28 +0000

    Initial Commit

---
+++ VCNodeTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VCNodeTest
+//
+//  Created by Duncan Lowrie on 26/11/2018.
+//  Copyright © 2018 Duncan Lowrie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ VCNodeTestTests/VCNodeTestTests.swift
@@ -0,0 +1,34 @@
+//
+//  VCNodeTestTests.swift
+//  VCNodeTestTests
+//
+//  Created by Duncan Lowrie on 26/11/2018.
+//  Copyright © 2018 Duncan Lowrie. All rights reserved.
+//
+
+import XCTest
+@testable import VCNodeTest
+
+class VCNodeTestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
